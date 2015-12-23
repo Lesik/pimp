c1120d666f784682a071c751eaa48afd6a1c19cf
Oles Pidgornyy <pidgornyy@informatik.uni-frankfurt.de>
2015-12-23 19:31:08 +0100

    load toolbar widget from glade

--- pimp.py
+++ pimp.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+from gi.repository import Gtk, Keybinder
+
+UI_FILE = "pimp.ui"
+
 class Pimp:
 
 	def __init__(self):
-		print("PIMP!")
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file(UI_FILE)
+		self.builder.connect_signals()
+
+		self.btn_open = self.builder.get_object('btn-open')
+		self.btn_save = self.builder.get_object('btn-save')
+		self.btn_undo = self.builder.get_object('btn-undo')
+		self.btn_redo = self.builder.get_object('btn-redo')
