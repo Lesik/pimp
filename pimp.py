2729e26baacb4dc29b3e7ace959b3606ab717233
Oles Pidgornyy <pidgornyy@informatik.uni-frankfurt.de>
2015-12-23 20:43:22 +0100

    add opeining file functionality

--- pimp.py
+++ pimp.py
@@ -17,11 +17,27 @@ class Pimp:
 		self.btn_redo = self.builder.get_object('btn-redo')
 
 		self.image = self.builder.get_object('image')
-		self.builder.get_object('window').show_all()
-		self.load_image()
 
-	def load_image(self):
-		self.image.set_from_file('Lenna.png')
+		self.window = self.builder.get_object('window')
+		self.window.show_all()
+
+	def load_image(self, filename):
+		self.image.set_from_file(filename)
+
+	def on_btn_open_clicked(self, widget):
+		dialog = Gtk.FileChooserDialog("Please choose a file",
+			self.window,
+			Gtk.FileChooserAction.OPEN,
+			(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+			Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+		response = dialog.run()
+		if response == Gtk.ResponseType.OK:
+			self.load_image(dialog.get_filename())
+		elif response == Gtk.ResponseType.CANCEL:
+			print("lol?")
+
+		dialog.destroy()
 
 	def on_window_destroy(self, window):
 		Gtk.main_quit()
